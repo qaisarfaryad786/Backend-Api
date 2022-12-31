7317c6a40f566a75d3a199cbad718127bc2789c6
Hamza Bin Shahzad <hamzabinshahzad@live.com>
2022-12-31 17:48:43 +0500

    added use CORS to avoid issues and added activity array in users schema

--- models/users.js
+++ models/users.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
         type:String,
         unique:true
     },
-    password:String
+    password:String,
+    activities: []
 });
 
 const register = new mongoose.model("register",userSchema);
